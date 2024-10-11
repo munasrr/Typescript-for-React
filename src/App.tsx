9512eded35c99a0017dcb708165ba75fa3b5b922
AnwarDev2 <Anwarabdi2090@gmail.com>
2024-10-11 20:59:50 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import './App.css'
 import {Person} from "./person";
 
 function App() {
-
-  <>
-    return
-     <Person name={'anwar'} age={23} isMarried={false} />
+return(
+      <>
+      
+  <Person name={'anwar'} age={23} isMarried={false} />
     {""}
      <Person name={'munasr'} age={28} isMarried={true} />
     {""}
-  </>;
+  </>
+    
+  
+  );
+   
   
 }
 
--- src/Person.tsx
+++ src/Person.tsx
@@ -1,11 +1,18 @@
+export interface Person{
+    name:string;
+    age:number;
+    isMarried:boolean;
 
-export const Person = ( props) =>{
+}
+
+
+export const Person = ( props:Person) =>{
 
     return (
       <div>
         <p>Name:{props.name}</p>
         <p>age:{props.age}</p>
-        <p>this person:{props.isMarried ? 'is married' : 'is single'}</p>
+        <p>this person{props.isMarried ? 'is married' : 'is single'}</p>
       </div>
     );
     
